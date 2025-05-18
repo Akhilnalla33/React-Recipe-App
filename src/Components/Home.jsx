261e5cf396432bbb62a1d17cc010b08d44789be3
Akhilnalla33 <166206096+Akhilnalla33@users.noreply.github.com>
2025-05-18 16:43:27 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';  // Import Slick Slider
-import './home.css';
+import './Home.css';
 
 const Home = ({ searchTerm }) => {
     const [recipes, setRecipes] = useState([]);
